1e498d7a7aac473201b5b30d6da13c751a24f411
sokcuri <sokcuri@gmail.com>
2020-05-16 16:52:10 +0900

    Proxy 세부 설정 추가

--- app/src/main/java/com/sokcuri/twimgspeedproxy/LittleProxy.kt
+++ app/src/main/java/com/sokcuri/twimgspeedproxy/LittleProxy.kt
@@ -1,16 +1,19 @@
 package com.sokcuri.twimgspeedproxy
 
 import android.content.Context
+import android.support.v7.preference.PreferenceManager
+import android.util.Log
+import com.google.common.net.HostAndPort
+import io.netty.channel.ChannelHandlerContext
+import io.netty.handler.codec.http.HttpObject
+import io.netty.handler.codec.http.HttpRequest
+import io.netty.handler.codec.http.HttpResponse
+import org.littleshoot.proxy.HttpFilters
+import org.littleshoot.proxy.HttpFiltersAdapter
+import org.littleshoot.proxy.HttpFiltersSourceAdapter
 import org.littleshoot.proxy.HttpProxyServer
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer
-import android.support.v7.preference.PreferenceManager
-import android.widget.Toast
-import com.google.common.net.HostAndPort
-import org.littleshoot.proxy.HttpFiltersAdapter
-import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.http.HttpRequest
-import org.littleshoot.proxy.HttpFilters
-import org.littleshoot.proxy.HttpFiltersSourceAdapter
+import org.littleshoot.proxy.impl.ThreadPoolConfiguration
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.UnknownHostException
@@ -46,6 +49,15 @@ class LittleProxy {
     private fun startProxyServer() {
         this.server = DefaultHttpProxyServer.bootstrap()
             .withPort(port)
+            .withAllowRequestToOriginServer(true)
+            .withConnectTimeout(1000)
+            .withIdleConnectionTimeout(10)
+            .withThreadPoolConfiguration(
+                ThreadPoolConfiguration()
+                .withAcceptorThreads(8)
+                .withClientToProxyWorkerThreads(16)
+                .withProxyToServerWorkerThreads(16)
+            )
             .withFiltersSource(object : HttpFiltersSourceAdapter() {
                 override fun filterRequest(originalRequest: HttpRequest, ctx: ChannelHandlerContext): HttpFilters {
                     return object : HttpFiltersAdapter(originalRequest) {
@@ -64,9 +76,15 @@ class LittleProxy {
                             val address = InetAddress.getByName(host)
                             return InetSocketAddress(address, port)
                         }
+
+                        override fun proxyToServerConnectionSSLHandshakeStarted() {
+                            Log.d("LittleProxy", "proxyToServerConnectionSSLHandshakeStarted")
+                        }
                     }
                 }
-            }).start()
+            })
+            .withUseDnsSec(true)
+            .start()
     }
 
     private fun stopProxyServer() {
@@ -82,6 +100,8 @@ class LittleProxy {
             try {
                 startProxyServer()
             } catch (exception: Exception) {
+
+                Log.d("startProxyServer", "does not started")
 
             }
         }.start()
